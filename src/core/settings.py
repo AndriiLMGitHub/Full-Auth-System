5e7294b5e4c0e1f7df36d56e7f1c8761e9cf6208
Mac <mac@MacBook-Air.local>
2024-06-27 22:37:11 +0300

    Email settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,8 +25,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "andreydecua@gmail.com"
-EMAIL_HOST_PASSWORD = "kvpb eadw ikri scec"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Application definition
 
